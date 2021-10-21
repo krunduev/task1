f9c002ba2660277303f07db9103a259f15065069
Konstantin Runduev <runduev@gmail.com>
2021-10-21 19:14:48 +0300

    uadd cosinus

--- main.go
+++ main.go
@@ -8,6 +8,6 @@ import (
 func main() {
 	var a float64 = 5.2
 	var b float64 = 1.1
-	var c = (math.Sin(a) + math.Cos(b)) / 5.2
+	var c = (math.Sin(a) + math.Cos(b)) / math.Cos(5.6)
 	fmt.Printf("Result: %v\n", c)
 }
